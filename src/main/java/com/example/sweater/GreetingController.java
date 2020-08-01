334c6b6c011d061c2bfaea290f232d12f9ff7316
marina2302 <marina23021995@gmail.com>
2020-08-01 21:31:26 +0300

    Add JPA with postgres

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,14 +1,20 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepos messageRepos;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
@@ -20,7 +26,30 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String,Object> model) {
-        model.put("some", "hello, it's sweater");
+        Iterable<Message> messages = messageRepos.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepos.save(message);
+        Iterable<Message> messages = messageRepos.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+        if (filter !=null && !filter.isEmpty()){
+       messages= messageRepos.findByTag(filter);
+
+        }else {
+            messages = messageRepos.findAll();
+        }
+        model.put("messages", messages);
         return "main";
     }
 }
