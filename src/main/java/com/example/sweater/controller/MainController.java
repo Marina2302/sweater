2d0ae041a3da1342abe3785aba202a5246c78cc9
marina2302 <marina23021995@gmail.com>
2020-08-04 22:17:41 +0300

    Add Spring Security

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepos;
@@ -8,48 +8,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepos messageRepos;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
-    public String main(Map<String,Object> model) {
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepos.findAll();
+
         model.put("messages", messages);
+
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
+
         messageRepos.save(message);
+
         Iterable<Message> messages = messageRepos.findAll();
+
         model.put("messages", messages);
+
         return "main";
     }
 
     @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
-        if (filter !=null && !filter.isEmpty()){
-       messages= messageRepos.findByTag(filter);
 
-        }else {
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepos.findByTag(filter);
+        } else {
             messages = messageRepos.findAll();
         }
+
         model.put("messages", messages);
+
         return "main";
     }
 }
---
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,39 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepos userRepos;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepos.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepos.save(user);
+
+        return "redirect:/login";
+    }
+}
