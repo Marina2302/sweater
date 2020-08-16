f4aaa5f900aac0d7dfd1f2ba6eaff3ac9aa7c69d
marina2302 <marina23021995@gmail.com>
2020-08-16 15:59:16 +0300

    Add mail verification

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration", "/static/**").permitAll()
+                .antMatchers("/", "/registration", "/static/**", "/active/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
